60fc621fad0fe7e926b30bce8bc4048992b241cd
cmreavis97 <cmreavis97@gmail.com>
2022-09-26 15:11:28 -0400

    First commit

---
+++ StaticExercise1/Program.cs
@@ -0,0 +1,11 @@
+﻿using StaticExercise1;
+
+
+Console.WriteLine("Your temperature converted to Fahrenheit:");
+//Input temp in degrees C
+Console.WriteLine(TempConverter.CelsiusToFahrenheit(20.00));
+
+Console.WriteLine();
+Console.WriteLine("Your temperature converted to Celsius:");
+//Input temp in degrees F
+Console.WriteLine(TempConverter.FahrenheitToCelsius(100));
---
+++ StaticExercise1/TempConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticExercise1
+{
+    public static class TempConverter
+    {
+        public static double FahrenheitToCelsius(double degreesF)
+        {
+            return (degreesF - 32) * 5 / 9;
+        }
+
+        public static double CelsiusToFahrenheit(double degreesC)
+        {
+            return (degreesC * 9) / 5 + 32;
+        }
+
+
+    }
+}
